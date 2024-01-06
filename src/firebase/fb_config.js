5a658d209ea29968967fb662ee87406b415b6bb3
Mary Danyliuk <marydanyliuk@gmail.com>
2024-01-06 23:17:22 +0200

    clean code

--- src/firebase/fb_config.js
+++ src/firebase/fb_config.js
@@ -1,15 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// import { getDatabase } from 'firebase/database';
+
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCfjG-RLGh4VqAL5XSxS9EG4IVK4R8XF-E',
   authDomain: 'filmoteka-23.firebaseapp.com',
@@ -25,7 +19,6 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// export const storage = getStorage(app);
-// export const analytics = getAnalytics(app);
+
 
 // console.log('Hello, Firestore!');
--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -13,25 +13,3 @@ function toggleBurgerMenu(event) {
     refs.mobileMenu.classList.toggle(`open`);
   }
 }
-
-// refs.body.classList.add(`lock`);
-// __________________________________________
-// function closeBurgerMenu(event) {
-//   event.preventDefault();
-//   refs.mobileMenu.classList.toggle(`open`);
-// }
-
-// const toggleMenu = () => {
-//   const isMenuOpen =
-//     openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//   mobileMenu.classList.toggle('is-open');
-
-//   const scrollLockMethod = !isMenuOpen
-//     ? 'disableBodyScroll'
-//     : 'enableBodyScroll';
-//   bodyScrollLock[scrollLockMethod](document.body);
-// };
-
-// openMenuBtn.addEventListener('click', toggleMenu);
-// closeMenuBtn.addEventListener('click', toggleMenu);
